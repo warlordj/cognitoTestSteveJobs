d0aebd13e2634008535cfbe436070d69cc2a677e
Jeremy Lee <jeremy@keenpayroll.ca>
2025-07-31 12:05:36 -0230

    deleted keys lol

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,5 +1,7 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js'
 
+// these are deleted, replace with your own
+
 const poolData = {
 UserPoolId: 'us-east-1_YwrWYiU85',
 ClientId: '42uqh1he4c0su5jehsvmcukv87',
